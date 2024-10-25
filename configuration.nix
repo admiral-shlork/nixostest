65e9d3dc601fe69822a1a0ea3b9ecf0ca9d33a0b
whatever <vvhtvr@protonmail.ch>
2024-10-25 20:39:22 +0100

    moar OCD

--- configuration.nix
+++ configuration.nix
@@ -4,8 +4,8 @@
   imports =
     [
       ./blank-configuration/hardware-configuration.nix
-      ./blank-configuration/networking-configuration.nix
       ./blank-configuration/misc-configuration.nix
+      ./blank-configuration/networking-configuration.nix
       ./blank-configuration/software-configuration.nix
       ./blank-configuration/storage-configuration.nix
     ];
