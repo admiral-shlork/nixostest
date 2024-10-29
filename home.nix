3918a86f178a74f8cd25a3b2d154adf43003438f
whatever <vvhtvr@protonmail.ch>
2024-10-29 15:41:00 +0000

    fixed home.nix

--- home.nix
+++ home.nix
@@ -2,16 +2,14 @@
 
 {
   # Home Manager configuration options go here
-  home.username = "whatever";
-
-  # Example configuration
-  home.packages = with pkgs; [
-    librewolf
-    lutris
-  ];
-
-  users.users.whatever = {
-    isNormalUser = true;
-    home = "/home/whatever";
+  home = {
+    username = "whatever";
+    homeDirectory = "/home/whatever";
+    stateVersion = "24.05";
+    packages = with pkgs; [
+      librewolf
+      lutris
+    ];
   };
+
 }
