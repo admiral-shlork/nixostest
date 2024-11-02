363e3e4b8f755f1157669105927ae19d4b63a769
whatever <vvhtvr@protonmail.ch>
2024-11-02 16:26:23 +0000

    changed printer label

--- blank-configuration/printing-configuration.nix
+++ blank-configuration/printing-configuration.nix
@@ -9,7 +9,7 @@
     ensureDefaultPrinter = "Brother_HL-L2370DN";
     ensurePrinters = [
       {
-        name = "Brother_HL-L2370DN";
+        name = "Brother HL-L2370DN";
         description = "Brother HL-L2370DN";
         #deviceUri = "dnssd://Brother%20HL-L2370DN%20series._ipp._tcp.local/?uuid=e3248000-80ce-11db-8000-b42200bfd1de";
         deviceUri = "socket://10.0.0.181";
