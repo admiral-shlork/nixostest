57a607320eb6179b11a6b6bddcf5d082a4c26c8d
whatever <vvhtvr@protonmail.ch>
2024-11-02 15:59:58 +0000

    added brlaser package

--- blank-configuration/software-configuration.nix
+++ blank-configuration/software-configuration.nix
@@ -32,6 +32,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    brlaser
     git
     gnome.dconf-editor
     gnomeExtensions.appindicator
