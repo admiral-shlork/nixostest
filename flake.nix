c061cdc8cbc983235f9d1a6f48daa5743f2bf59d
whatever <vvhtvr@protonmail.ch>
2024-11-02 12:23:19 +0000

    fixed flake definition for hosaka

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
       pkgs = nixpkgs.legacyPackages.${system};
     in {
       nixosConfigurations = {
-        nixostest = lib.hosaka {
+        hosaka = lib.nixosSystem {
           inherit system;
           modules = [ 
             ./configuration.nix
