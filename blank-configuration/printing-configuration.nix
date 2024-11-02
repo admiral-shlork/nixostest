8103695a066027c8fc757775d502562ed5b31d1c
whatever <vvhtvr@protonmail.ch>
2024-11-02 16:07:58 +0000

    using socket for printing

--- blank-configuration/printing-configuration.nix
+++ blank-configuration/printing-configuration.nix
@@ -11,7 +11,8 @@
       {
         name = "Brother_HL-L2370DN";
         description = "Brother HL-L2370DN";
-        deviceUri = "dnssd://Brother%20HL-L2370DN%20series._ipp._tcp.local/?uuid=e3248000-80ce-11db-8000-b42200bfd1de";
+        #deviceUri = "dnssd://Brother%20HL-L2370DN%20series._ipp._tcp.local/?uuid=e3248000-80ce-11db-8000-b42200bfd1de";
+        deviceUri = "socket://10.0.0.181";
         model = "drv:///brlaser.drv/brl2370d.ppd";
       }
     ];
